a77519b81078e43401f4293ca948070f52d6b8c6
HG_ORO <h.g.orozco@gmail.com>
2019-04-29 10:02:33 -0700

    Updates

---
+++ precache-manifest.f6aa8c3282e08846f1d08af36848ce0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d7ada222444f6874f69bdab97ae580b",
+    "url": "/clickyClickyGame/index.html"
+  },
+  {
+    "revision": "f306f6a8f8e054d8578f",
+    "url": "/clickyClickyGame/static/css/main.8eb4ab8d.chunk.css"
+  },
+  {
+    "revision": "f78b2180fd5fd8373e40",
+    "url": "/clickyClickyGame/static/js/2.4da7185e.chunk.js"
+  },
+  {
+    "revision": "f306f6a8f8e054d8578f",
+    "url": "/clickyClickyGame/static/js/main.280f6cd7.chunk.js"
+  },
+  {
+    "revision": "b8f1152e641a5ab06fbf",
+    "url": "/clickyClickyGame/static/js/runtime~main.13980492.js"
+  }
+]);
